620461236322f5dfb6f64ed61526f65d9672a299
lakshay1208 <76162193+lakshay1208@users.noreply.github.com>
2023-08-06 14:39:13 +0530

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import streamlit as st
-from utils import parse_pdf, embed_text, get_answer
+from transformers import pipeline
 
-st.header("Doc QA")
-uploaded_file = st.file_uploader("Upload a pdf", type=["pdf"])
+@st.cache(allow_output_mutation=True)
+def load_qa_model():
+    model = pipeline("question-answering")
+    return model
 
-if uploaded_file is not None:
-    index = embed_text(parse_pdf(uploaded_file))
-    query = st.text_area("Ask a question about the document")
-    button = st.button("Submit")
-    if button:
-        st.write(get_answer(index, query))
+qa = load_qa_model()
+st.title("Ask Questions about your Text")
+sentence = st.text_area('Please paste your article :', height=30)
+question = st.text_input("Questions from this article?")
+button = st.button("Get me Answers")
+max = st.sidebar.slider('Select max', 50, 500, step=10, value=150)
+min = st.sidebar.slider('Select min', 10, 450, step=10, value=50)
+do_sample = st.sidebar.checkbox("Do sample", value=False)
+with st.spinner("Discovering Answers.."):
+    if button and sentence:
+        answers = qa(question=question, context=sentence)
+        st.write(answers['answer'])
